4b302fe5f3ab3029b28cbe22d88b57864104199d
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-08-29 02:24:27 -0300

    Atualizando arquivos

---
+++ Carlos/ProjetoThomas.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace Carlos
+{
+    partial class ProjetoThomas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Botoes = new System.Windows.Forms.GroupBox();
+            this.btnExc = new System.Windows.Forms.Button();
+            this.btnInc = new System.Windows.Forms.Button();
+            this.Formulario = new System.Windows.Forms.GroupBox();
+            this.rbtSim = new System.Windows.Forms.RadioButton();
+            this.rbtNao = new System.Windows.Forms.RadioButton();
+            this.btnCanc = new System.Windows.Forms.Button();
+            this.btnSal = new System.Windows.Forms.Button();
+            this.txtCusto = new System.Windows.Forms.TextBox();
+            this.txtDist = new System.Windows.Forms.TextBox();
+            this.txtNivDor = new System.Windows.Forms.TextBox();
+            this.txtData = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lstInf = new System.Windows.Forms.ListView();
+            this.projetoThomasBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.programBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.projetoThomasBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.projetoThomasBindingSource2 = new System.Windows.Forms.BindingSource(this.components);
+            this.Botoes.SuspendLayout();
+            this.Formulario.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.projetoThomasBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.programBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.projetoThomasBindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.projetoThomasBindingSource2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Botoes
+            // 
+            this.Botoes.Controls.Add(this.btnExc);
+            this.Botoes.Controls.Add(this.btnInc);
+            this.Botoes.Location = new System.Drawing.Point(383, 12);
+            this.Botoes.Name = "Botoes";
+            this.Botoes.Size = new System.Drawing.Size(405, 51);
+            this.Botoes.TabIndex = 1;
+            this.Botoes.TabStop = false;
+            // 
+            // btnExc
+            // 
+            this.btnExc.Location = new System.Drawing.Point(124, 19);
+            this.btnExc.Name = "btnExc";
+            this.btnExc.Size = new System.Drawing.Size(75, 23);
+            this.btnExc.TabIndex = 1;
+            this.btnExc.Text = "Excluir";
+            this.btnExc.UseVisualStyleBackColor = true;
+            this.btnExc.Click += new System.EventHandler(this.BtnExc_Click);
+            // 
+            // btnInc
+            // 
+            this.btnInc.Location = new System.Drawing.Point(6, 19);
+            this.btnInc.Name = "btnInc";
+            this.btnInc.Size = new System.Drawing.Size(75, 23);
+            this.btnInc.TabIndex = 0;
+            this.btnInc.Text = "Incluir";
+            this.btnInc.UseVisualStyleBackColor = true;
+            this.btnInc.Click += new System.EventHandler(this.BtnInc_Click);
+            // 
+            // Formulario
+            // 
+            this.Formulario.Controls.Add(this.rbtSim);
+            this.Formulario.Controls.Add(this.rbtNao);
+            this.Formulario.Controls.Add(this.btnCanc);
+            this.Formulario.Controls.Add(this.btnSal);
+            this.Formulario.Controls.Add(this.txtCusto);
+            this.Formulario.Controls.Add(this.txtDist);
+            this.Formulario.Controls.Add(this.txtNivDor);
+            this.Formulario.Controls.Add(this.txtData);
+            this.Formulario.Controls.Add(this.label7);
+            this.Formulario.Controls.Add(this.label6);
+            this.Formulario.Controls.Add(this.label5);
+            this.Formulario.Controls.Add(this.label3);
+            this.Formulario.Controls.Add(this.label1);
+            this.Formulario.Location = new System.Drawing.Point(383, 69);
+            this.Formulario.Name = "Formulario";
+            this.Formulario.Size = new System.Drawing.Size(405, 288);
+            this.Formulario.TabIndex = 2;
+            this.Formulario.TabStop = false;
+            // 
+            // rbtSim
+            // 
+            this.rbtSim.AutoSize = true;
+            this.rbtSim.Location = new System.Drawing.Point(243, 92);
+            this.rbtSim.Name = "rbtSim";
+            this.rbtSim.Size = new System.Drawing.Size(42, 17);
+            this.rbtSim.TabIndex = 13;
+            this.rbtSim.TabStop = true;
+            this.rbtSim.Text = "Sim";
+            this.rbtSim.UseVisualStyleBackColor = true;
+            // 
+            // rbtNao
+            // 
+            this.rbtNao.AutoSize = true;
+            this.rbtNao.Location = new System.Drawing.Point(124, 92);
+            this.rbtNao.Name = "rbtNao";
+            this.rbtNao.Size = new System.Drawing.Size(45, 17);
+            this.rbtNao.TabIndex = 3;
+            this.rbtNao.TabStop = true;
+            this.rbtNao.Text = "Nao";
+            this.rbtNao.UseVisualStyleBackColor = true;
+            // 
+            // btnCanc
+            // 
+            this.btnCanc.Location = new System.Drawing.Point(168, 209);
+            this.btnCanc.Name = "btnCanc";
+            this.btnCanc.Size = new System.Drawing.Size(75, 23);
+            this.btnCanc.TabIndex = 2;
+            this.btnCanc.Text = "Cancelar";
+            this.btnCanc.UseVisualStyleBackColor = true;
+            this.btnCanc.Click += new System.EventHandler(this.BtnCanc_Click);
+            // 
+            // btnSal
+            // 
+            this.btnSal.Location = new System.Drawing.Point(42, 209);
+            this.btnSal.Name = "btnSal";
+            this.btnSal.Size = new System.Drawing.Size(75, 23);
+            this.btnSal.TabIndex = 3;
+            this.btnSal.Text = "Salvar";
+            this.btnSal.UseVisualStyleBackColor = true;
+            this.btnSal.Click += new System.EventHandler(this.BtnSal_Click);
+            // 
+            // txtCusto
+            // 
+            this.txtCusto.Location = new System.Drawing.Point(124, 40);
+            this.txtCusto.Name = "txtCusto";
+            this.txtCusto.Size = new System.Drawing.Size(261, 20);
+            this.txtCusto.TabIndex = 12;
+            // 
+            // txtDist
+            // 
+            this.txtDist.Location = new System.Drawing.Point(124, 66);
+            this.txtDist.Name = "txtDist";
+            this.txtDist.Size = new System.Drawing.Size(261, 20);
+            this.txtDist.TabIndex = 11;
+            // 
+            // txtNivDor
+            // 
+            this.txtNivDor.Location = new System.Drawing.Point(124, 114);
+            this.txtNivDor.Name = "txtNivDor";
+            this.txtNivDor.Size = new System.Drawing.Size(261, 20);
+            this.txtNivDor.TabIndex = 10;
+            // 
+            // txtData
+            // 
+            this.txtData.Location = new System.Drawing.Point(124, 13);
+            this.txtData.Name = "txtData";
+            this.txtData.Size = new System.Drawing.Size(261, 20);
+            this.txtData.TabIndex = 3;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(6, 40);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(34, 13);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Custo";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 64);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(51, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Distancia";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 88);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Captura";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Nivel dor";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Data";
+            // 
+            // lstInf
+            // 
+            this.lstInf.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.lstInf.FullRowSelect = true;
+            this.lstInf.GridLines = true;
+            this.lstInf.HideSelection = false;
+            this.lstInf.Location = new System.Drawing.Point(14, 12);
+            this.lstInf.MultiSelect = false;
+            this.lstInf.Name = "lstInf";
+            this.lstInf.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lstInf.Size = new System.Drawing.Size(349, 345);
+            this.lstInf.TabIndex = 3;
+            this.lstInf.UseCompatibleStateImageBehavior = false;
+            this.lstInf.View = System.Windows.Forms.View.Details;
+            this.lstInf.SelectedIndexChanged += new System.EventHandler(this.LstInf_SelectedIndexChanged);
+            this.lstInf.DoubleClick += new System.EventHandler(this.LstInf_DoubleClick);
+            // 
+            // projetoThomasBindingSource
+            // 
+            this.projetoThomasBindingSource.DataSource = typeof(Carlos.ProjetoThomas);
+            // 
+            // programBindingSource
+            // 
+            this.programBindingSource.DataSource = typeof(Carlos.Program);
+            // 
+            // projetoThomasBindingSource1
+            // 
+            this.projetoThomasBindingSource1.DataSource = typeof(Carlos.ProjetoThomas);
+            // 
+            // projetoThomasBindingSource2
+            // 
+            this.projetoThomasBindingSource2.DataSource = typeof(Carlos.ProjetoThomas);
+            // 
+            // ProjetoThomas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstInf);
+            this.Controls.Add(this.Formulario);
+            this.Controls.Add(this.Botoes);
+            this.Name = "ProjetoThomas";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Acme";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Botoes.ResumeLayout(false);
+            this.Formulario.ResumeLayout(false);
+            this.Formulario.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.projetoThomasBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.programBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.projetoThomasBindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.projetoThomasBindingSource2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox Botoes;
+        private System.Windows.Forms.Button btnExc;
+        private System.Windows.Forms.Button btnInc;
+        private System.Windows.Forms.GroupBox Formulario;
+        private System.Windows.Forms.RadioButton rbtSim;
+        private System.Windows.Forms.RadioButton rbtNao;
+        private System.Windows.Forms.Button btnCanc;
+        private System.Windows.Forms.Button btnSal;
+        private System.Windows.Forms.TextBox txtCusto;
+        private System.Windows.Forms.TextBox txtDist;
+        private System.Windows.Forms.TextBox txtNivDor;
+        private System.Windows.Forms.TextBox txtData;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.BindingSource projetoThomasBindingSource;
+        private System.Windows.Forms.BindingSource programBindingSource;
+        private System.Windows.Forms.BindingSource projetoThomasBindingSource1;
+        private System.Windows.Forms.BindingSource projetoThomasBindingSource2;
+        public System.Windows.Forms.ListView lstInf;
+    }
+}
+
---
+++ Carlos/ProjetoThomas.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+using System.IO;
+
+namespace Carlos
+{
+
+    
+    public partial class ProjetoThomas : Form
+    {
+
+        private static SQLiteConnection SQLiteConnection;
+        public ProjetoThomas()
+        {
+            InitializeComponent();
+            try
+            {
+                
+                if (File.Exists("acme.sqlite"))
+                {
+                    ProjetoThomas.DbAcme();
+                    MessageBox.Show("Conexão estabelecida");
+                }
+                else
+                {
+                 
+                    using (var cmd = DbAcme().CreateCommand())
+                    {
+                        cmd.CommandText = "CREATE TABLE IF NOT EXISTS TB_VOO(Id_Voo INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, Data_Voo Varchar , Custo Numeric(10,2), Distancia int, Captura Char(1), Nivel_Dor )";
+                        cmd.ExecuteNonQuery();
+                        MessageBox.Show("Tabela criada com sucesso");
+
+                    }
+
+
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        private static SQLiteConnection DbAcme()
+        {
+            SQLiteConnection = new SQLiteConnection("Data Source = acme.sqlite");
+            SQLiteConnection.Open();
+            return SQLiteConnection;
+        }
+
+
+        private void BtnSal_Click(object sender, EventArgs e)
+        {
+            int ids = Convert.ToInt32(lstInf.SelectedItems[0].SubItems[0].Text);
+
+                using (var cmd = DbAcme().CreateCommand())
+                {
+
+                    cmd.CommandText = "update TB_VOO set Data_Voo = ? , Captura = ?, Nivel_Dor = ? WHERE Id_Voo = '" + ids + "' ";
+
+                    cmd.Parameters.AddWithValue("@Data_Voo", txtData.Text);
+
+                    if (rbtNao.Checked == true)
+                    {
+                        cmd.Parameters.AddWithValue("@Captura", "N");
+
+                    }
+                    else
+                    {
+                        cmd.Parameters.AddWithValue("@Captura", "S");
+                    }
+
+
+
+                    cmd.Parameters.AddWithValue("@Nivel_Dor", txtNivDor.Text);
+
+                    cmd.CommandType = CommandType.Text;
+                    cmd.ExecuteNonQuery();
+
+                    lstInf.Items.Clear();
+                    AtualizarLista();
+                }
+            
+        }
+        private void Form1_Load(object sender, EventArgs e)
+            {
+            
+            try
+            {
+                using (var cmd = DbAcme().CreateCommand())
+                {
+                      cmd.CommandText = "SELECT Id_Voo, Data_Voo, Captura, Nivel_Dor FROM TB_VOO";
+                      SQLiteDataReader dr = cmd.ExecuteReader();
+
+                for (int i = 0; i < dr.FieldCount; i++)
+                {
+                    ColumnHeader ch = new ColumnHeader();
+                    ch.TextAlign = HorizontalAlignment.Center;
+                    ch.Width = 87;
+                    ch.Text = dr.GetName(i);
+                    lstInf.Columns.Add(ch);
+                }
+
+                    ListViewItem item;
+
+                    while (dr.Read())
+                    {
+                        item = new ListViewItem();
+                        item.Text = dr.GetValue(0).ToString();
+
+
+                        for (int i = 1; i < dr.FieldCount; i++)
+                        {
+                            item.SubItems.Add(dr.GetValue(i).ToString());
+                        }
+                        lstInf.Items.Add(item);
+                    }
+
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            btnSal.Enabled = false ;
+            btnCanc.Enabled = false;
+
+        }
+        private void BtnExc_Click(object sender, EventArgs e)
+        {
+            
+            int id = Convert.ToInt32(lstInf.SelectedItems[0].SubItems[0].Text);
+
+           
+                using (var cmd = DbAcme().CreateCommand())
+                {
+                    cmd.CommandText = "delete from TB_VOO where Id_Voo ='" + id + "'";
+
+                    cmd.ExecuteNonQuery();
+                    lstInf.Items.Clear();
+                    AtualizarLista();
+                }
+            }
+        
+        private void BtnInc_Click(object sender, EventArgs e)
+        {
+                                   
+
+            try
+            {
+                using (var cmd = DbAcme().CreateCommand())
+                {
+                    
+                    if (rbtNao.Checked == true)
+                    {
+                        cmd.Parameters.AddWithValue("@Captura", "N");
+
+                    }
+                    else
+                    {
+                        cmd.Parameters.AddWithValue("@Captura", "S");
+                    }
+
+                    cmd.CommandText = "INSERT INTO TB_Voo(Data_Voo, Captura, Nivel_Dor ) values (@Data_Voo, @Captura, @Nivel_Dor)";
+                    cmd.Parameters.AddWithValue("@Data_Voo", txtData.Text);
+                    cmd.Parameters.AddWithValue("@Nivel_Dor", txtNivDor.Text);
+                    cmd.ExecuteNonQuery();
+
+                    
+
+                    txtData.Text = "";
+                    txtNivDor.Text = "";
+                    rbtNao.Checked = false;
+                    rbtSim.Checked = false;
+
+                    lstInf.Items.Clear();
+                    AtualizarLista();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            
+
+
+        
+        }
+
+        private void LstInf_DoubleClick(object sender, EventArgs e)
+        {
+            btnCanc.Enabled = true;
+            btnSal.Enabled = true;
+
+
+
+            using (var cmd = DbAcme().CreateCommand())
+            {
+
+
+                SQLiteDataReader dr;
+
+                int ida = Convert.ToInt32(lstInf.SelectedItems[0].SubItems[0].Text);
+
+               
+                    cmd.CommandText = "SELECT Data_Voo, Captura, Nivel_Dor FROM TB_VOO WHERE Id_Voo = '" + ida + "'";
+                    cmd.Parameters.AddWithValue("@Data_Voo", txtData.Text);
+                    cmd.Parameters.AddWithValue("@Nivel_Dor", txtNivDor.Text);
+
+                    cmd.CommandType = CommandType.Text;
+
+                    dr = cmd.ExecuteReader();
+                    dr.Read();
+                    txtData.Text = dr.GetString(0);
+                    if (lstInf.SelectedItems[0].SubItems[2].Text == "S")
+                    {
+                        rbtSim.Checked = true;
+                    }
+                    else
+                    {
+                        rbtSim.Checked = false;
+                        rbtNao.Checked = true;
+
+                    }
+
+                    txtNivDor.Text = dr.GetString(2);
+            }
+                lstInf.Items.Clear();
+                AtualizarLista();
+
+                      
+        }
+
+        private void AtualizarLista()
+        {
+            ListViewItem item;
+
+            using (var cmd = DbAcme().CreateCommand())
+            {
+                cmd.CommandText = "SELECT Id_Voo, Data_Voo, Captura, Nivel_Dor FROM TB_VOO";
+                SQLiteDataReader dr = cmd.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    item = new ListViewItem();
+                    item.Text = dr.GetValue(0).ToString();
+
+
+                    for (int i = 1; i < dr.FieldCount; i++)
+                    {
+                        item.SubItems.Add(dr.GetValue(i).ToString());
+                    }
+                    lstInf.Items.Add(item);
+                }
+
+
+
+            }
+        }
+            private void BtnCanc_Click(object sender, EventArgs e)
+        {
+            btnCanc.Enabled = false;
+            btnSal.Enabled = false;
+
+            txtData.Clear();
+            rbtNao.Checked = false;
+            rbtSim.Checked = false;
+            txtNivDor.Clear();
+        }
+
+        private void LstInf_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
